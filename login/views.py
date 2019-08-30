44a55755f97d7d930ae478ed6af5f3d367700b5b
lamborghini1993 <1323242382@qq.com>
2019-08-30 15:10:01 +0800

    7.2 修改视图

--- login/views.py
+++ login/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 
 from . import models
+from . import forms
 
 
 def index(request):
@@ -9,18 +10,26 @@ def index(request):
 
 def login(request):
     if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        message = "用户名或密码不匹配!"
-        try:
-            user = models.User.objects.get(name=username)
-        except:
-            return render(request, "login/login.html", {"message": message})
-        if user.password == password:
-            return redirect("/index/")
-        else:
-            return render(request, "login/login.html", {"message": message})
-    return render(request, "login/login.html")
+        login_form = forms.UserForm(request.POST)
+        message = "请检查填写的内容！"
+        
+        if login_form.is_valid():
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            message = "用户名或密码不匹配!"
+            try:
+                user = models.User.objects.get(name=username)
+            except:
+                return render(request, "login/login.html", locals())
+
+            if user.password == password:
+                return redirect("/index/")
+            return render(request, "login/login.html", locals())
+
+        return render(request, "login/login.html", locals())
+
+    login_form = forms.UserForm()
+    return render(request, "login/login.html", locals())
 
 
 def register(request):
