5c61c4c5d8c9e9255e9d123e122bc1bec7b53846
lamborghini1993 <1323242382@qq.com>
2019-08-30 16:00:55 +0800

    10.3 实现注册视图

--- login/views.py
+++ login/views.py
@@ -5,9 +5,9 @@ from . import forms
 
 
 def index(request):
-    if request.session.get('is_login', None):
+    if request.session.get("is_login", None):
         return render(request, "login/index.html")
-    return redirect('/login/')
+    return redirect("/login/")
 
 
 def login(request):
@@ -27,9 +27,9 @@ def login(request):
                 return render(request, "login/login.html", locals())
 
             if user.password == password:
-                request.session['is_login'] = True
-                request.session['user_id'] = user.id
-                request.session['user_name'] = user.name
+                request.session["is_login"] = True
+                request.session["user_id"] = user.id
+                request.session["user_name"] = user.name
                 return redirect("/index/")
             return render(request, "login/login.html", locals())
 
@@ -40,11 +40,47 @@ def login(request):
 
 
 def register(request):
-    return render(request, "login/register.html")
+    if request.session.get("is_login", None):
+        return redirect("/index/")  # 不允许重复登录
+
+    if request.method == "POST":
+        register_form = forms.RegisterForm(request.POST)
+        message = "请检查填写的内容！"
+        if register_form.is_valid():
+            password1 = register_form.cleaned_data.get("password1")
+            password2 = register_form.cleaned_data.get("password2")
+            if password1 != password2:
+                message = "两次输入的密码不相同!"
+                return render(request, "login/register.html", locals())
+
+            username = register_form.cleaned_data.get("username")
+            same_name_user = models.User.objects.filter(name=username)
+            if same_name_user:
+                message = "用户名已经存在"
+                return render(request, "login/register.html", locals())
+
+            email = register_form.cleaned_data.get("email")
+            same_email_user = models.User.objects.filter(email=email)
+            if same_email_user:
+                message = "该邮箱已经被注册了！"
+                return render(request, "login/register.html", locals())
+
+            new_user = models.User()
+            new_user.name = username
+            new_user.password = password1
+            new_user.email = email
+            new_user.sex = register_form.cleaned_data.get("sex")
+            new_user.save()
+            return redirect("/login/")
+
+        return render(request, "login/register.html", locals())
+
+    register_form = forms.RegisterForm()
+    return render(request, "login/register.html", locals())
 
 
 def logout(request):
-    if request.session.get('is_login', None):
+    if request.session.get("is_login", None):
         request.session.flush()
         return redirect("/login/")
     return redirect("/login/")
