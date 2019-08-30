aa33160cdae11a46a026d9a6f2acc3c9c7d45113
lamborghini1993 <1323242382@qq.com>
2019-08-30 14:39:54 +0800

    6.4 添加登录错误提示信息

--- login/views.py
+++ login/views.py
@@ -11,12 +11,15 @@ def login(request):
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
+        message = "用户名或密码不匹配!"
         try:
             user = models.User.objects.get(name=username)
         except:
-            return render(request, "login/login.html")
+            return render(request, "login/login.html", {"message": message})
         if user.password == password:
             return redirect("/index/")
+        else:
+            return render(request, "login/login.html", {"message": message})
     return render(request, "login/login.html")
 
 
